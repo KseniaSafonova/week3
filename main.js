0bc0f9f4dd77f0b2505d219a67321cfe2172fb16
KseniaSafonova <kseniatsn@gmail.com>
2022-11-07 15:57:26 +0300

    07/11

--- main.js
+++ main.js
@@ -1,61 +1,21 @@
 
-let text = document.getElementById('text');
-let darkThemeButton = document.getElementById('darkThemeButton');
-let lightThemeButton = document.getElementById('lightThemeButton');
-let block = document.getElementById('block');
-
-let backgroundLight = '#F5F5F5';
-let textColorLight = '#191970';
-
-let backgroundDark = '#2F4F4F';
-let textColorDark = '#DCDCDC';
+let textarea = document.getElementById('textarea');
 
 document.addEventListener("DOMContentLoaded", function (event) {
+    let restoredTextarea = localStorage.getItem('textarea');
 
-    let restoredBackgroundColor = localStorage.getItem('backgroundColor');
-    let restoredTextColor = localStorage.getItem('textColor');
-    let restoredLightButtonVisibility = localStorage.getItem('lightButtonVisibility');
-    let restoredDarkButtonVisibility = localStorage.getItem('darkButtonVisibility');
-
-    if (restoredBackgroundColor != null && restoredTextColor != null && restoredLightButtonVisibility != null && restoredDarkButtonVisibility != null) {
-        block.style.backgroundColor = restoredBackgroundColor;
-        block.style.color = restoredTextColor;
-        lightThemeButton.style.display = restoredLightButtonVisibility;
-        darkThemeButton.style.display = restoredDarkButtonVisibility;
+    if (restoredTextarea != null) {
+        textarea.innerHTML = restoredTextarea;
     }
 
-    else { //светлая тема по умолчанию
-        block.style.backgroundColor = backgroundLight;
-        block.style.color = textColorLight;
-        lightThemeButton.style.display = 'none';
-        darkThemeButton.style.display = 'block';
+    else {
+        textarea.innerHTML = '';
     }
 })
 
-darkThemeButton.addEventListener('click', function () {
-    block.style.backgroundColor = backgroundDark;
-    block.style.color = textColorDark;
-    lightThemeButton.style.display = 'block';
-    darkThemeButton.style.display = 'none';
-
-    localStorage.setItem('backgroundColor', backgroundDark);
-    localStorage.setItem('textColor', textColorDark);
-    localStorage.setItem('lightButtonVisibility', 'block');
-    localStorage.setItem('darkButtonVisibility', 'none');
-})
-
-lightThemeButton.addEventListener('click', function () {
-    block.style.backgroundColor = backgroundLight;
-    block.style.color = textColorLight;
-    lightThemeButton.style.display = 'none';
-    darkThemeButton.style.display = 'block';
-
-    localStorage.setItem('backgroundColor', backgroundLight);
-    localStorage.setItem('textColor', textColorLight);
-    localStorage.setItem('lightButtonVisibility', 'none');
-    localStorage.setItem('darkButtonVisibility', 'block');
-})
-
+textarea.oninput = function () {
+    localStorage.setItem('textarea', textarea.value);
+}
 
 
 
